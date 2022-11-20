a86677649d4d03e036c9a2e0e49d4bc4422e6920
tanaka <tanaka@tanakanoMacBook-Air.local>
2022-11-20 16:19:37 +0900

    add: refactor react-tutorial

---
+++ dev-app/react/refactor/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as ReactDOM from 'react-dom/client';
+import Game from "./components/Game";
+import "./index.css";
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement)
+root.render(<Game />);
