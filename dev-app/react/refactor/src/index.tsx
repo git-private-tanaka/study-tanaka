28772c9019baf72d493a3cd8c6b7e74c86ca33da
tanaka <tanaka@tanakanoMacBook-Air.local>
2022-11-21 23:01:57 +0900

    add: eslint(ts)

--- dev-app/react/refactor/src/index.tsx
+++ dev-app/react/refactor/src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import * as ReactDOM from 'react-dom/client';
-import Game from "./components/Game";
-import "./index.css";
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import Game from './components/Game'
+import './index.css'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById('root')
+if (rootElement == null) throw new Error('Failed to find the root element')
 const root = ReactDOM.createRoot(rootElement)
-root.render(<Game />);
+root.render(<Game />)
